bce3246fef0376560d120994a53c32f97390c2c2
Ryumina <alsk464@gmail.com>
2018-09-19 12:43:41 +0900

    풀페이지 연결 후 우측 고정 버튼 , 로고/footer 로고 클릭시 jquery 작동 안됨

--- js/main6.js
+++ js/main6.js
@@ -97,10 +97,8 @@ $(function () {
 	$(".main_page_3_right_btn").find("li").eq(1).click(function(){
 		b++;
 		b=b%3;
-		if(b==1){ //b는 보이는 것 그대로 1이다.
-			a=-2; //a에 -2라는 값을 대입한다.
-			//a의 값이 2인 상태에서 a==='2' 를 하게되면 flase 임 왜냐면 a는 숫자 2와 문자열 2 라는 변수의 type까지
-			// 비교해서 더 정확한 결과를 받을 수 있음. 
+		if(b==1){
+			a=-2; 
 			$("#main_page_3_slide").animate().stop();
 			$("#main_page_3_slide").animate({"left":"-100vw"},1000,"swing");
 		}else if(b==2){
@@ -120,14 +118,14 @@ $(function(){
 	$('.right_btn a').click(function (e) {
     e.preventDefault()
     var id = $(this).attr('href')
-    // var scrollTop = id === '#' ? 0 : $(id).offset().top
-	var scrollTop
+    var scrollTop = id === '#' ? 0 : $(id).offset().top
+	/* 위를 풀어서 쓴 것 var scrollTop
 	if (id === '#') {
 		scrollTop = 0
 	} else {
 		scrollTop = $(id).offset().top
-	}
-    $("html").animate({
+	}*/
+    $("html,body").animate({
       scrollTop: scrollTop
     }, 700, "swing")
   });
@@ -140,25 +138,4 @@ $(function(){
 			$("html").animate({scrollTop:scrollTop},700,"swing");
 	});
 
-});
-
-$(function(){
-	$(window).on("mousewheel",function(){
-				var main_1=$("html").hasClass("wheel_1"); //hasClass의 대답은 true / flase 2가지임
-				var main_2=$("html").hasClass("wheel_2");
-				var main_3=$("html").hasClass("wheel_3");
-				var main_4=$("html").hasClass("wheel_4");
-				var main_5=$("html").hasClass("wheel_5");
-				
-				if(main_1==true){ //만약 a라는 클래스를 가지고 있다면~
-					$("#btn").find("li").removeClass("click"); 
-					$("#btn").find("li").eq(0).addClass("click");
-				}else if(b==true){ //만약 b라는 클래스를 가지고 있다면~
-					$("#btn").find("li").removeClass("click");
-					$("#btn").find("li").eq(1).addClass("click");
-				}else if(c==true){ //만약 c라는 클래스를 가지고 있다면~
-					$("#btn").find("li").removeClass("click");
-					$("#btn").find("li").eq(2).addClass("click");
-				}
-			});
 });
